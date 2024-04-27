87bb756727774c414529c1cb1ecf93c922c3130b
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 16:10:38 +0300

    fix code smells

--- src/main/java/by/abakumova/weatherjava/controller/ErrorController.java
+++ src/main/java/by/abakumova/weatherjava/controller/ErrorController.java
@@ -4,12 +4,16 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
+@RequestMapping("/error")
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
-    @RequestMapping("/error")
+
+    @GetMapping
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
